fe40a62109213b5407956c49a431c96ae48d5838
Jace Gonzales <jacegonzales25@gmail.com>
2023-07-20 14:09:29 +0800

    Finished setting up Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors"; 
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 console.log("Hello Typescript!");
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log("Server listening at localhost:8080");
+});
