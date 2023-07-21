2ac6cf605d4ce0e8cd652da2e280e9643a4fb1ce
Jace Gonzales <jacegonzales25@gmail.com>
2023-07-21 21:14:38 +0800

    Finished first controller!

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,6 +5,8 @@ import 'dotenv/config';
 const SECRET = process.env.SECRET;
 // Random method to return a random string of characters.
 export const random = () => crypto.randomBytes(128).toString('base64');
+
+// authentication method that takes in a salt and password and returns a crypted string.
 export const authentication = (salt: string, password: string) => {
     return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
 };
