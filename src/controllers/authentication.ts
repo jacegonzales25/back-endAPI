087aa07f5605954617ee6d7f1f126d44de68d072
Jace Gonzales <jacegonzales25@gmail.com>
2023-07-27 20:20:49 +0800

    Finished login controller with annotations

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -6,8 +6,38 @@ import { random, authentication } from '../helpers';
 export const login = async (req: express.Request, res: express.Response) => {
     try {
         // The destructured data with the body of the request
-        const  {username, email, password } = req.body;
+        const  { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+
+        // The select method will select the fields with the salt and the password. Since it is salted and concatenated.
+        // This also enables the access of the salt and the password in expectedHash.
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+        if (!user) {
+            console.log("User not found");
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password !== expectedHash) {
+            return res.sendStatus(403);
+        }
+
+        // Salter and Hasher
+        const salt = random();
+        // The sessionToken is the salted and hashed user id, that allows unique access to the user.
+        user.authentication.sessionToken = authentication(salt, user.id.toString());
+
+        await user.save();
+
+        res.cookie('JACE-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/'})
         
+        return res.status(200).json(user).end();
+
     } catch(error) {
         console.log(error);
         res.sendStatus(400);
--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ app.use(cors({
 
 app.use(compression());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 
